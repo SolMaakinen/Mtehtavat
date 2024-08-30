269e0617cfe4be96b6e189f5db74fce84b532bc4
SolMaakinen <solluxugo@gmail.com>
2024-08-30 12:50:15 +0300

    jjep

--- Ohjelmisto1Tehtävä3.1.py
+++ Ohjelmisto1Tehtävä3.1.py
@@ -1,3 +1,3 @@
-kpituus = int(input("Anna kuhan pituus sentteinä: "))
+kpituus = float(input("Anna kuhan pituus sentteinä: "))
 if kpituus <37:
     print(f"Laske kuha takaisin järveen, sen pitää kasvaa vielä {37-kpituus} senttiä ")
