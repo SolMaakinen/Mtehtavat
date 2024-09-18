dccffcf5eafff411fdc28336288e9d7c22860e3a
SolMaakinen <solluxugo@gmail.com>
2024-09-18 22:27:33 +0300

    jjep

--- Ohjelmisto1Tehtävä5.4.py
+++ Ohjelmisto1Tehtävä5.4.py
@@ -4,5 +4,5 @@ for luku in range(5):
     kaupungit.append(input("Anna kaupungin nimi "))
 
 print("Kaupunkilista: ")
-for kaupungit in kaupungit:
-    print(kaupungit)
+for kaupunki in kaupungit:
+    print(kaupunki)
